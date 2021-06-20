6cbd48a8e53828088fd455b3337e0b7e3abc7941
iamtyk <312715093@qq.com>
2021-06-20 14:07:31 +0800

    MergeTwoLists

--- firstHomeWork/MergeTwoLists.go
+++ firstHomeWork/MergeTwoLists.go
@@ -6,6 +6,30 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	//测试提交2
-	return &ListNode{}
+	if nil == l1 {
+		return l2
+	} else if nil == l2 {
+		return l1
+	}
+
+	head := &ListNode{}
+	tail := head
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			tail.Next = l1
+			l1 = l1.Next
+		} else {
+			tail.Next = l2
+			l2 = l2.Next
+		}
+
+		tail = tail.Next
+	}
+	if l1 != nil {
+		tail.Next = l1
+	}
+	if l2 != nil {
+		tail.Next = l2
+	}
+	return head.Next
 }
