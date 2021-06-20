a04637bf1f04e68b575910215961c45e554d5190
iamtyk <312715093@qq.com>
2021-06-20 13:41:39 +0800

    Sort Files

---
+++ firstHomeWork/MergeTwoLists.go
@@ -0,0 +1,11 @@
+package firstHomeWork
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	//测试提交2
+	return &ListNode{}
+}
--- firstHomeWork/PluseOne.go
+++ firstHomeWork/PluseOne.go
@@ -1,10 +1,5 @@
 package firstHomeWork
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func PlusOne(digits []int) []int {
 	career := 1
 	for i := len(digits) - 1; i >= 0; i-- {
@@ -22,9 +17,4 @@ func PlusOne(digits []int) []int {
 	}
 	ans = append(ans, digits...)
 	return ans
-}
-
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	//测试提交2
-	return &ListNode{}
 }
