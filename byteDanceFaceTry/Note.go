7cf511e2705f27a60e6024ce81bfbe642a9d6a56
iamtyk <312715093@qq.com>
2021-06-24 01:30:26 +0800

    [bytedance]corpFlightBookings

--- byteDanceFaceTry/Note.go
+++ byteDanceFaceTry/Note.go
@@ -1,5 +1,10 @@
 package byteDanceFaceTry
 
+import (
+	"GeekAlgorithmWork/Global"
+	"math"
+)
+
 func Quick_sort(nums []int, l, r int) {
 	if l+1 >= r {
 		return
@@ -46,4 +51,58 @@ func Merge_sort(nums, temp []int, l, r int) {
 		nums[i] = temp[i]
 	}
 
+}
+
+//最大子序和
+func maxSubArray(nums []int) int {
+	pre := make([]int, len(nums)+1)
+	for i, val := range nums {
+		pre[i+1] = pre[i] + val
+	}
+	preMin := make([]int, len(nums)+1)
+	for i, _ := range nums {
+		preMin[i+1] = Global.Min(preMin[i], pre[i+1])
+	}
+	ans := math.MinInt32
+	for i := 1; i <= len(nums); i++ {
+		//i之前的 -->j<=i-1
+		ans = Global.Max(ans, pre[i]-preMin[i-1])
+	}
+	return ans
+}
+
+//差分
+/*
+	差分和前缀和是两个互逆运算
+	差分数组，求前缀和就变成原数组
+	前缀和数组，求差分变成原数组
+	cal_b(cal_sum(A))=A
+*/
+/* 任何对于区间的操作，可以转化为两个关键点（事件）
+事件的影响从l开始，在r+1处消失
+累加影响得到答案
+l +d    r+1   -d
+
+  1  2  3  4  5
+ 10    -10
+    20    -20
+    25          (-25)
+ 10 45 -10 -20 0
+ 10 55 45  25 25
+*/
+//1109. 航班预订统计
+func corpFlightBookings(bookings [][]int, n int) []int {
+	delta := make([]int, n+2)
+	for _, book := range bookings {
+		fir := book[0]
+		last := book[1]
+		val := book[2]
+		delta[fir] += val
+		delta[last+1] -= val
+	}
+	sum := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		sum[i] = sum[i-1] + delta[i]
+	}
+	return sum[1:]
 }
