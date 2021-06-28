60cd1ce75cf67e7fc36d9a45e40b1aa9526afd64
iamtyk <312715093@qq.com>
2021-06-29 00:54:01 +0800

    [Week2]LRU

--- main.go
+++ main.go
@@ -2,27 +2,56 @@ package main
 
 import (
 	"GeekAlgorithmWork/Global"
-	"GeekAlgorithmWork/byteDanceFaceTry"
-	"GeekAlgorithmWork/firstHomeWork"
+	"GeekAlgorithmWork/week2"
 	"fmt"
 	"sort"
-	"strconv"
 )
 
 func main() {
-	inputSlice := []int{1, 2, 3}
-	ans := firstHomeWork.PlusOne(inputSlice)
-	fmt.Println(ans)
-	//int转string
-	strconv.Itoa(1)
-	inputSlice = []int{3, 1, 2}
-	byteDanceFaceTry.Quick_sort(inputSlice, 0, len(inputSlice))
-	fmt.Println(inputSlice)
-	inputSlice = []int{3, 1, 2}
-	byteDanceFaceTry.Merge_sort(inputSlice, make([]int, len(inputSlice)), 0, len(inputSlice))
-	fmt.Println(inputSlice)
-	fmt.Println(strconv.Atoi("1"))
-	fmt.Println(twoSum(inputSlice, 5))
+	/*
+		inputSlice := []int{1, 2, 3}
+		ans := firstHomeWork.PlusOne(inputSlice)
+		fmt.Println(ans)
+		//int转string
+		strconv.Itoa(1)
+		inputSlice = []int{3, 1, 2}
+		byteDanceFaceTry.Quick_sort(inputSlice, 0, len(inputSlice))
+		fmt.Println(inputSlice)
+		inputSlice = []int{3, 1, 2}
+		byteDanceFaceTry.Merge_sort(inputSlice, make([]int, len(inputSlice)), 0, len(inputSlice))
+		fmt.Println(inputSlice)
+		fmt.Println(strconv.Atoi("1"))
+		fmt.Println(twoSum(inputSlice, 5))
+	*/
+	/*
+		["LRUCache","get","put","get","put","put","get","get"]
+		[[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
+
+		["LRUCache","put","put","get","put","get","put","get","get","get"]
+		[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+	*/
+	fmt.Println("LRUTest")
+	//cmdStr :=[]string{"LRUCache","put","put","get","put","get","put","get","get","get"}
+	//param := [][]int{{2},{1,1},{2,2},{1},{3,3},{2},{4,4},{1},{3},{4}}
+	cmdStr := []string{"LRUCache", "get", "put", "get", "put", "put", "get", "get"}
+	param := [][]int{{2}, {2}, {2, 6}, {1}, {1, 5}, {1, 2}, {1}, {2}}
+	var cache week2.LRUCache
+	for i, cm := range cmdStr {
+		switch cm {
+		case "LRUCache":
+			cache = week2.Constructor(param[i][0])
+			fmt.Println("null")
+			break
+		case "get":
+			fmt.Println(cache.Get(param[i][0]))
+			break
+		case "put":
+			cache.Put(param[i][0], param[i][1])
+			fmt.Println("null")
+			break
+		}
+	}
+
 }
 
 func twoSum(nums []int, target int) []int {
---
+++ week2/LRU.go
@@ -0,0 +1,89 @@
+package week2
+
+import "container/list"
+
+//用list包做，但是耗时特别长，应该是value.([]int)的时候出问题了
+//试了一下，也没快多少
+type LRUCache struct {
+	cacheList *list.List            //list包中，头前结点是尾指针，
+	refectMap map[int]*list.Element //[]int{key, value}
+	capactiy  int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		cacheList: list.New(),
+		refectMap: map[int]*list.Element{},
+		capactiy:  capacity,
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	val, ok := this.refectMap[key]
+	if !ok {
+		return -1
+	}
+	this.cacheList.MoveToFront(val)
+	return val.Value.([]int)[1]
+}
+
+//先插入，再删除才可以，否则键值一样的时候就直接移位了
+func (this *LRUCache) Put(key int, value int) {
+	if val, ok := this.refectMap[key]; ok {
+		val.Value = []int{key, value}
+		this.cacheList.MoveToFront(val)
+		return
+	}
+	this.cacheList.PushFront([]int{key, value})
+	this.refectMap[key] = this.cacheList.Front()
+	if this.capactiy < this.cacheList.Len() {
+		delete(this.refectMap, this.cacheList.Back().Value.([]int)[0])
+		this.cacheList.Remove(this.cacheList.Back())
+	}
+}
+
+//转long版本，可能几个转换的地方还是很多
+/*
+type LRUCache struct {
+	cacheList *list.List//list包中，头前结点是尾指针，
+	refectMap map[int64]*list.Element//[]int{key, value}
+	capactiy int
+}
+
+const   constantName = 100000
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		cacheList : list.New(),
+		refectMap :  map[int64]*list.Element{},
+		capactiy:capacity,
+	}
+}
+
+
+func (this *LRUCache) Get(key int) int {
+	val, ok := this.refectMap[int64(key)]
+	if !ok{
+		return -1
+	}
+	this.cacheList.MoveToFront(val)
+	return int(val.Value.(int64)%constantName)
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+	if val, ok := this.refectMap[int64(key)];ok{
+		val.Value=int64(key*constantName+value)
+		this.cacheList.MoveToFront(val)
+		return
+	}
+	this.cacheList.PushFront(int64( key*constantName+value))
+	this.refectMap[int64(key)]=this.cacheList.Front()
+	if this.capactiy < this.cacheList.Len(){
+		delete(this.refectMap, this.cacheList.Back().Value.(int64)/constantName)
+		this.cacheList.Remove(this.cacheList.Back())
+	}
+}
+
+
+*/
